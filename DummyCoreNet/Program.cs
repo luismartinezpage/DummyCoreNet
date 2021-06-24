d102909fbfd587cf07d0c704fbc082663fe862f0
root <lmartinez@viewnext.com>
2021-06-24 14:23:21 +0200

    Added new project core net

---
+++ DummyCoreNet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DummyCoreNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = string.Empty;
+            Console.WriteLine("¿Cuál es tu nombre?");
+            name = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("mmm.... porque no quieres poner tu nombre? :) ");
+            }
+            else
+            {
+                Console.WriteLine(string.Format("Hola {0}, que tengas un buen día", name));
+            }
+
+            Console.WriteLine("<pulsa cualquier tecla para salir.");
+            Console.ReadKey();
+            
+        }
+    }
+}
